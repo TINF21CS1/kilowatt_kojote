39a9745f908e91f39d144cd947c9eb0c2eb03f15
Petzys <87223648+Petzys@users.noreply.github.com>
2023-11-01 11:22:08 +0100

    Added flask auth boilerplate

--- messstellenbetreiberportal/__init__.py
+++ messstellenbetreiberportal/__init__.py
@@ -5,7 +5,7 @@ def create_app(test_config=None):
     # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
-        SECRET_KEY='dev',
+        SECRET_KEY='dev', # TODO: Change this before deploying
         DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
     )
 
@@ -26,5 +26,11 @@ def create_app(test_config=None):
     @app.route('/hello')
     def hello():
         return 'Hello, World!'
+    
+    from . import db
+    db.init_app(app)
+
+    from . import auth
+    app.register_blueprint(auth.bp)
 
     return app
