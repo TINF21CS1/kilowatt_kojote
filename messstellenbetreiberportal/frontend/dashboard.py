8de0c535cddce8e5041f14522566a3323908efd3
Petzys <87223648+Petzys@users.noreply.github.com>
2023-11-08 15:14:08 +0100

    Add Flask to requirements and update templates
    - Added dashboard mock

--- messstellenbetreiberportal/frontend/dashboard.py
+++ messstellenbetreiberportal/frontend/dashboard.py
@@ -6,4 +6,21 @@ bp = Blueprint('dashboard', __name__, url_prefix='/')
 
 @bp.route('', methods=['GET'])
 def dashboard():
-    return render_template('smartmeter/dashboard.html')
+    # Define Plot Data 
+    labels = [
+        'January',
+        'February',
+        'March',
+        'April',
+        'May',
+        'June',
+    ]
+ 
+    data = [0, 10, 15, 8, 22, 18, 25]
+ 
+    # Return the components to the HTML template 
+    return render_template(
+        template_name_or_list='smartmeter/dashboard.html',
+        data=data,
+        labels=labels,
+    )
