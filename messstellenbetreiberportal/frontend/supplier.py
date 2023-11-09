b3ab3e20173545f5a454d323f21274e4e45bce81
Petzys <87223648+Petzys@users.noreply.github.com>
2023-11-09 17:47:33 +0100

    added new supplier form

--- messstellenbetreiberportal/frontend/supplier.py
+++ messstellenbetreiberportal/frontend/supplier.py
@@ -10,4 +10,14 @@ def supplier():
 
 @bp.route('/add', methods=['GET', 'POST'])
 def add():
+    if request.method == 'POST':
+        name = request.form['name']
+        street = request.form['notes']
+        # TODO: Add code to add supplier to database
+        return redirect(url_for('supplier.supplier'))
+    
+    return render_template('supplier/add.html')
+
+@bp.route('/assign', methods=['GET', 'POST'])
+def assign():
     return render_template('supplier/add.html')
