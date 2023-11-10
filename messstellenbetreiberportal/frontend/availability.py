b1e7024b99a4d94af6615ddb1061648c5b9f627a
Petzys <87223648+Petzys@users.noreply.github.com>
2023-11-10 12:04:33 +0100

    Add smartmeter uptime calculation and display on dashboard
    - Added test data generators

---
+++ messstellenbetreiberportal/frontend/availability.py
@@ -0,0 +1,57 @@
+import time
+from collections import OrderedDict
+
+INTERVAL = 60*15 # 15 minutes
+TIMEFRAME = 60*60*24*30 # 30 days
+
+def get_uptime(readings:list, interval:int = INTERVAL, timeframe:int = TIMEFRAME, current_time = int(time.time())) -> dict:
+    """Calculates the uptime of a smartmeter from a list of readings.
+
+    Args:
+        readings (list): List of readings
+
+    Returns:
+        dict(int, int): Dictionary with the uptime for each interval
+    """
+
+    if len(readings) == 0:
+        return [1]
+
+    # Iterate through the whole timeframe in intervals of 15 minutes and calculate the uptime
+    uptime = OrderedDict()
+    for i in range(current_time - timeframe, current_time, interval):
+        # Check if there is a reading in the current interval
+        if any(reading['timestamp'] >= i and reading['timestamp'] < i + interval for reading in readings):
+            uptime[i] = 1
+        else:
+            uptime[i] = 0
+
+    return uptime
+
+def uptime_smartmeters(smartmeter:list, interval:int = INTERVAL, timeframe:int = TIMEFRAME, current_time:int = int(time.time())) -> dict:
+    """Calculates the uptime of a list of smartmeters.
+
+    Args:
+        smartmeter (list): List of smartmeters
+
+    Returns:
+        dict(int, float): Dictionary with the uptime for each interval
+    """
+
+    # Get the uptime for each smartmeter
+    uptime = OrderedDict()
+    for meter in smartmeter:
+        uptime[meter['uuid']] = get_uptime(meter['data'], interval, timeframe, current_time)
+
+    # Get average uptime for each interval
+    average_uptime = OrderedDict()
+    for i in range(current_time - timeframe, current_time, interval):
+        average_uptime[i] = sum(uptime[meter['uuid']][i] for meter in smartmeter) / len(smartmeter)
+
+    return average_uptime
+
+# Print some example data
+if __name__ == "__main__":
+    from testdata import get_smartmeter_test_list
+    smartmeters = get_smartmeter_test_list(10)
+    print(uptime_smartmeters(smartmeters))
--- messstellenbetreiberportal/frontend/dashboard.py
+++ messstellenbetreiberportal/frontend/dashboard.py
@@ -2,25 +2,39 @@ from flask import (
     Blueprint, flash, g, redirect, render_template, request, session, url_for
 )
 
+from datetime import datetime
+from .testdata import get_smartmeter_test_list
+from .availability import uptime_smartmeters, TIMEFRAME, INTERVAL
+import time
+
 bp = Blueprint('dashboard', __name__, url_prefix='/')
 
 @bp.route('', methods=['GET'])
 def dashboard():
+    # Create list of smartmeters
+    smartmeters = get_smartmeter_test_list(10)
+
+    current_time = int(time.time())
+    # Get uptime for all smartmeters
+    uptime = uptime_smartmeters(smartmeters, current_time=current_time)
+
+    # Average uptime over last 24 hours
+    day_interval = 60*60*24
+    
+    # Get average uptime for last 24 hours
+    last_24_hour_uptime = {key: value for key, value in uptime.items() if key >= current_time-day_interval}
+
+    average_uptime = sum(last_24_hour_uptime.values()) / len(last_24_hour_uptime)
+
     # Define Plot Data 
-    labels = [
-        'January',
-        'February',
-        'March',
-        'April',
-        'May',
-        'June',
-    ]
- 
-    data = [0, 10, 15, 8, 22, 18, 25]
+    labels = [datetime.utcfromtimestamp(stamp).strftime('%Y-%m-%d %H:%M:%S') for stamp in uptime.keys()]
+    data = list(uptime.values())
  
     # Return the components to the HTML template 
     return render_template(
         template_name_or_list='smartmeter/dashboard.html',
         data=data,
         labels=labels,
+        current_uptime = round(data[-1], 4),
+        average_uptime = round(average_uptime, 4),
     )
