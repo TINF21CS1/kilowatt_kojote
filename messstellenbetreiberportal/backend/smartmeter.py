8ef1382e088a8616f4de653234e764ef28e243a0
Tayhm <>
2023-11-14 19:59:41 +0100

    added handling for /smartmeter/data endpoint and now using json schemas for validation

--- messstellenbetreiberportal/backend/smartmeter.py
+++ messstellenbetreiberportal/backend/smartmeter.py
@@ -1,23 +1,72 @@
 from flask import Flask, request, jsonify, Blueprint
 from .db import db_manager
+import jsonschema
+import time
 
 bp = Blueprint("smartmeter_backend", __name__, url_prefix="/api/smartmeter")
+
+smartmeter_register_schema = {
+    "type": "object",
+    "properties": {
+    "type": {
+        "description": "Type of the smartmeter",
+        "type": "integer",
+        "minimum": 0,
+        "maximum": 2
+        }
+    },
+    "required": ["type"]
+}
+
+smartmeter_data_schema = {
+    "type": "array",
+    "items": {
+    "type": "object",
+    "properties": {
+        "timestamp": {
+        "description": "Timestamp of the submitted reading",
+        "type": "integer",
+        "minimum": 1672527600
+        },
+        "reading": {
+        "description": "The reading of the reader at the timestamp",
+        "type": "integer"
+        }
+    },
+    "required": ["timestamp", "reading"]
+    },
+    "minItems": 1
+}
 
 @bp.route("/register", methods=["POST"])
 def register():
     json = request.get_json()
 
-    if not "type" in json or type(reader_type := json["type"]) != int:
+    try:
+        jsonschema.validate(json, smartmeter_register_schema)
+    except jsonschema.ValidationError:
         return "", 400
 
     sim_uuid = "test"
 
-    db_manager.smartmeter_register(sim_uuid, reader_type)
+    db_manager.smartmeter_register(sim_uuid, json["type"])
     
-    return jsonify(["test", 2, True])
+    return "", 200
 
 @bp.route("/data", methods=["POST"])
 def data():
     json = request.get_json()
-    print(json)
-    return jsonify(["test", 2, True])
+    
+    try:
+        jsonschema.validate(json, smartmeter_data_schema)
+    except jsonschema.ValidationError:
+        return "", 400
+
+    sim_uuid = "test"
+
+    actual_timestamp = int(time.time())
+
+    for element in json:
+        db_manager.smartmeter_data(sim_uuid, element["timestamp"], actual_timestamp, element["reading"])
+
+    return "", 200
