25d85eb16c0bf717958b979a5d50d99dfa69d36a
Tayhm <>
2023-11-15 15:14:17 +0100

    Added json schema for frontend_supplier_assign function

--- messstellenbetreiberportal/backend/frontend.py
+++ messstellenbetreiberportal/backend/frontend.py
@@ -1,21 +1,44 @@
 import jsonschema
 
+MAX_SUPPLIER_NAME_LEN = 500
+MIN_SUPPLIER_NAME_LEN = 2
+MAX_SUPPLIER_NOTES_LEN = 5000
+
 frontend_supplier_add_schema = {
     "type": "object",
     "properties": {
         "name": {
             "description": "Name of the supplier",
             "type": "string",
-            "minLength": 2,
-            "maxLength": 500    # Completely arbitrary, just to make sure that it's not too large for the TEXT Type in the DB
+            "minLength": MIN_SUPPLIER_NAME_LEN,
+            "maxLength": MAX_SUPPLIER_NAME_LEN    # Completely arbitrary, just to make sure that it's not too large for the TEXT Type in the DB
         },
         "notes": {
             "description": "Additional notes regarding the supplier",
             "type": "string",
-            "maxLength": 5000   # Again, completely arbitrary
+            "maxLength": MAX_SUPPLIER_NOTES_LEN   # Again, completely arbitrary
         }
     },
     "required": ["name", "notes"]
+}
+
+frontend_supplier_assign_schema = {
+
+    "type": "object",
+    "properties": {
+        "supplier": {
+            "description": "Name of the supplier to which the smartmeter should be added",
+            "type": "string",
+            "minLength": MIN_SUPPLIER_NAME_LEN,
+            "maxLength": MAX_SUPPLIER_NAME_LEN
+        },
+        "smartmeter": {
+            "description": "The serial number of the smartmeter that should be added to the supplier",
+            "type": "string"
+        }
+    },
+    "required": ["supplier", "smartmeter"]
+
 }
 
 def frontend_smartmeter() -> list(dict):
