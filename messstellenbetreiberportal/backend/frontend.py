76d5a29d9b3ea611b88f356bef0ada30c4109940
Tayhm <>
2023-11-15 15:04:56 +0100

    Added json schema for frontend_supplier_add function

--- messstellenbetreiberportal/backend/frontend.py
+++ messstellenbetreiberportal/backend/frontend.py
@@ -1,3 +1,22 @@
+import jsonschema
+
+frontend_supplier_add_schema = {
+    "type": "object",
+    "properties": {
+        "name": {
+            "description": "Name of the supplier",
+            "type": "string",
+            "minLength": 2,
+            "maxLength": 500    # Completely arbitrary, just to make sure that it's not too large for the TEXT Type in the DB
+        },
+        "notes": {
+            "description": "Additional notes regarding the supplier",
+            "type": "string",
+            "maxLength": 5000   # Again, completely arbitrary
+        }
+    },
+    "required": ["name", "notes"]
+}
 
 def frontend_smartmeter() -> list(dict):
     return
