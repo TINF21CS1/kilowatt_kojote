dc79497d800d54300e2eacd2ba3dd463c2ba825d
Petzys <87223648+Petzys@users.noreply.github.com>
2023-11-16 13:36:53 +0100

    Add waitress to requirements.txt and remove unnecessary config from __init__.py

--- messstellenbetreiberportal/__init__.py
+++ messstellenbetreiberportal/__init__.py
@@ -4,10 +4,6 @@ import os
 def create_app(test_config=None):
     # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
-    app.config.from_mapping(
-        SECRET_KEY='dev', # TODO: Change this before deploying
-        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
-    )
 
     if test_config is None:
         # load the instance config, if it exists, when not testing
