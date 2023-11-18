4bd33eb345131c61a6ad606f9e2b6c24f237c9d9
Petzys <87223648+Petzys@users.noreply.github.com>
2023-11-18 11:58:57 +0100

    Refactor supplier.py to handle database errors

--- messstellenbetreiberportal/frontend/supplier.py
+++ messstellenbetreiberportal/frontend/supplier.py
@@ -1,6 +1,9 @@
 from flask import (
-    Blueprint, flash, g, redirect, render_template, request, session, url_for
+    Blueprint, redirect, render_template, request, session, url_for
 )
+import logging
+
+logger = logging.getLogger(__name__)
 
 bp = Blueprint('supplier', __name__, url_prefix='/supplier')
 
@@ -13,7 +16,14 @@ def add():
     if request.method == 'POST':
         name = request.form['name']
         street = request.form['notes']
-        # TODO: Add code to add supplier to database
+        
+        try: 
+            # TODO: Add code to add supplier to database
+            pass
+        except ValueError as e:
+            logger.exception(e)
+            return render_template('error.html', errors=str(e))
+        
         return redirect(url_for('supplier.supplier'))
     
     return render_template('supplier/add.html')
@@ -23,57 +33,70 @@ def assign():
     if request.method == 'POST':
         smartmeter = request.form['smartmeter']
         supplier = request.form['supplier']
-        # TODO: Add code to add assignment to database
+        
+        try:
+            # TODO: Add code to add assignment to database
+            pass
+        except ValueError as e:
+            logger.exception(e)
+            return render_template('error.html', errors=str(e))
+        
         return redirect(url_for('supplier.supplier'))
     
-    # Create list of smartmeters
-    smartmeters = [
-        {
-            "uuid": "123456789012sgf34567890123456789012",
-            "type": "Smartmeter",
-            "location": "Keller",
-            "supplier": "Stadtwerke",
-            "reading": "1234567890",
-            "usage": "1234567890",
-            "avg_usage": "1234567890"
-        },
-        {
-            "uuid": "23452345",
-            "type": "Smartmeter",
-            "location": "Keller",
-            "supplier": "LOLOL",
-            "reading": "1234567890",
-            "usage": "1234567890",
-            "avg_usage": "1234567890"
-        },
-        {
-            "uuid": "qwerqwerqwer",
-            "type": "Smartmeter",
-            "location": "Keller",
-            "supplier": "LOLOL",
-            "reading": "1234567890",
-            "usage": "1234567890",
-            "avg_usage": "1234567890"
-        },
-    ]
+    else:
+        try:
+            # TODO: Add code to get smartmeters and suppliers from database
+            pass
+        except ValueError as e:
+            logger.exception(e)
+            return render_template('error.html', errors=str(e))
 
-    # Create list of suppliers
-    suppliers = [
-        {
-            "uuid": "123456789012sgf34567890123456789012",
-            "name": "Stadtwerke",
-        },
-        {
-            "uuid": "23452345",
-            "name": "LOLOL",
-        },
-        {
-            "uuid": "qwerqwerqwer",
-            "name": "LOLOL",
-        }
-    ]
+        smartmeters = [
+            {
+                "uuid": "123456789012sgf34567890123456789012",
+                "type": "Smartmeter",
+                "location": "Keller",
+                "supplier": "Stadtwerke",
+                "reading": "1234567890",
+                "usage": "1234567890",
+                "avg_usage": "1234567890"
+            },
+            {
+                "uuid": "23452345",
+                "type": "Smartmeter",
+                "location": "Keller",
+                "supplier": "LOLOL",
+                "reading": "1234567890",
+                "usage": "1234567890",
+                "avg_usage": "1234567890"
+            },
+            {
+                "uuid": "qwerqwerqwer",
+                "type": "Smartmeter",
+                "location": "Keller",
+                "supplier": "LOLOL",
+                "reading": "1234567890",
+                "usage": "1234567890",
+                "avg_usage": "1234567890"
+            },
+        ]
 
-    return render_template(
-        suppliers = suppliers,
-        smartmeters = smartmeters,
-        template_name_or_list='supplier/assign.html')
+        suppliers = [
+            {
+                "uuid": "123456789012sgf34567890123456789012",
+                "name": "Stadtwerke",
+            },
+            {
+                "uuid": "23452345",
+                "name": "LOLOL",
+            },
+            {
+                "uuid": "qwerqwerqwer",
+                "name": "LOLOL",
+            }
+        ]
+
+        return render_template(
+            suppliers = suppliers,
+            smartmeters = smartmeters,
+            template_name_or_list='supplier/assign.html')
