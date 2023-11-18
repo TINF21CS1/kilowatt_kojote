0475071c629276adc64912088afb6f07990e4d04
Petzys <87223648+Petzys@users.noreply.github.com>
2023-11-18 11:58:03 +0100

    Added future backend endpoints and error handling

--- messstellenbetreiberportal/frontend/dashboard.py
+++ messstellenbetreiberportal/frontend/dashboard.py
@@ -1,31 +1,37 @@
 from flask import (
-    Blueprint, flash, g, redirect, render_template, request, session, url_for
+    Blueprint, render_template,
 )
 
 from datetime import datetime
 from .testdata import get_smartmeter_test_list
-from .availability import uptime_smartmeters, TIMEFRAME, INTERVAL
+from .availability import uptime_smartmeters
 from .errors import errors_smartmeter
 import time
+import logging
+#from ..backend.frontend import frontend_smartmeter
+
+logger = logging.getLogger(__name__)
 
 bp = Blueprint('dashboard', __name__, url_prefix='/')
 
 @bp.route('', methods=['GET'])
 def dashboard():
     # Create list of smartmeters
-    smartmeters = get_smartmeter_test_list(10)
+    try:
+        smartmeters = get_smartmeter_test_list(10) # TODO: Replace with real data
+        #smartmeters = frontend_smartmeter()
+    except ValueError as e:
+        logger.exception(e)
+        return render_template('error.html', errors=str(e))
 
-    current_time = int(time.time())
     # Get uptime for all smartmeters
+    current_time = int(time.time())
     uptime = uptime_smartmeters(smartmeters, current_time=current_time)
 
-    # Average uptime over last 24 hours
-    day_interval = 60*60*24
-    
     # Get average uptime for last 24 hours
+    day_interval = 60*60*24
     last_24_hour_uptime = {key: value for key, value in uptime.items() if key >= current_time-day_interval}
-
-    average_uptime = sum(last_24_hour_uptime.values()) / len(last_24_hour_uptime)
+    average_uptime = sum(last_24_hour_uptime.values()) / len(last_24_hour_uptime) if len(last_24_hour_uptime) > 0 else 0
 
     # Location Data from string
     for smartmeter in smartmeters:
