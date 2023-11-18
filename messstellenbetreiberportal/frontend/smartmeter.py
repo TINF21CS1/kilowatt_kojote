431eacd7503920bd64024a023e3bc898c715c4b0
Petzys <87223648+Petzys@users.noreply.github.com>
2023-11-18 11:58:09 +0100

    Refactor smartmeter.py to include revoke functionality and display smartmeter usage details

--- messstellenbetreiberportal/frontend/smartmeter.py
+++ messstellenbetreiberportal/frontend/smartmeter.py
@@ -1,6 +1,16 @@
 from flask import (
-    Blueprint, flash, g, redirect, render_template, request, session, url_for
+    Blueprint, render_template, request,
 )
+
+from datetime import datetime
+from .testdata import get_smartmeter_test_list
+from .availability import uptime_smartmeters
+from .errors import errors_smartmeter
+#from ..backend.frontend import frontend_smartmeter_revoke, frontend_smartmeter, frontend_smartmeter_reading
+import time
+import logging
+
+logger = logging.getLogger(__name__)
 
 bp = Blueprint('smartmeter', __name__, url_prefix='/smartmeter')
 
@@ -13,57 +23,67 @@ def smartmeter():
     
 def overview():
     if request.method == 'POST' and "id" in request.form:
-        # TODO: Insert Revoke Functionality
-        pass
+        try:
+            #frontend_smartmeter_revoke(request.form["id"])
+            # TODO: Insert Revoke Functionality
+            pass
+        except ValueError as e:
+            logger.exception(e)
+            return render_template('error.html', errors=str(e))
 
-    # Random input for testing smartmeter overview
-    smartmeters = [
-        {
-            "uuid": "123456789012sgf34567890123456789012",
-            "type": "Smartmeter",
-            "location": "Keller",
-            "supplier": "Stadtwerke",
-            "reading": "1234567890",
-            "usage": "1234567890",
-            "avg_usage": "1234567890"
-        },
-        {
-            "uuid": "23452345",
-            "type": "Smartmeter",
-            "location": "Keller",
-            "supplier": "LOLOL",
-            "reading": "1234567890",
-            "usage": "1234567890",
-            "avg_usage": "1234567890"
-        },
-        {
-            "uuid": "qwerqwerqwer",
-            "type": "Smartmeter",
-            "location": "Keller",
-            "supplier": "LOLOL",
-            "reading": "1234567890",
-            "usage": "1234567890",
-            "avg_usage": "1234567890"
-        },
-    ]
+    try:
+        smartmeters = get_smartmeter_test_list(100) # TODO: Change to real data
+        #smartmeters = frontend_smartmeter()
+    except ValueError as e:
+        logger.exception(e)
+        return render_template('error.html', errors=str(e))
+    
+    smartmeters = smartermeter_usage(smartmeters)
 
-    return render_template('smartmeter/overview.html', smartmeters=smartmeters*100)
+    return render_template('smartmeter/overview.html', smartmeters=smartmeters)
 
 def detail():
+    # Create list of smartmeters
+    try:
+        smartmeter = get_smartmeter_test_list(1) # TODO: Replace with real data
+        #smartmeter = {
+        #    "uuid" : request.args.get("id"),
+        #    "data" : frontend_smartmeter_reading(request.args.get("id"))
+        #}
+    except ValueError as e:
+        logger.exception(e)
+        return render_template('error.html', errors=str(e))
+
+    smartmeter = smartermeter_usage(smartmeter)
+
+    # Get uptime for smartmeter
+    current_time = int(time.time())
+    uptime = uptime_smartmeters(smartmeter, current_time=current_time)
+
     # Define Plot Data 
-    labels = [
-        'January',
-        'February',
-        'March',
-        'April',
-        'May',
-        'June',
-    ]
+    labels = [datetime.utcfromtimestamp(stamp).strftime('%Y-%m-%d %H:%M:%S') for stamp in uptime.keys()]
+    data = list(uptime.values())
  
-    data = [0, 10, 15, 8, 22, 18, 25]
+    # Return the components to the HTML template 
     return render_template(
         uuid=request.args.get("id"),
         data=data,
         labels=labels,
-        template_name_or_list='smartmeter/detail.html')
-    
+        template_name_or_list='smartmeter/detail.html',
+        smartmeter=smartmeter[0],
+        errors=errors_smartmeter(smartmeter),
+        unix_to_datetime=unix_to_datetime,
+        )
+
+def unix_to_datetime(unix_time):
+    return datetime.utcfromtimestamp(unix_time).strftime('%Y-%m-%d %H:%M:%S')
+
+# Expand smartmeter data with usage details
+def smartermeter_usage(smartmeters:list) -> list:
+    for smartmeter in smartmeters:
+        for i, data in enumerate(smartmeter["data"]):
+            # Check if previous list entry exists
+            if len(smartmeter["data"]) > i+1:
+                # Calculate usage
+                data["usage"] = round((data["reading"] - smartmeter["data"][i+1]["reading"])*1000 / (data["timestamp"] - smartmeter["data"][i+1]["timestamp"]), 2)
+    return smartmeters
