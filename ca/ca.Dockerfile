502a527e6bcdc9096f72bd1638f7595842387bec
Julian Lemmerich <jle@cirosec.de>
2023-11-20 16:45:21 +0100

    fix(ca): clean output
    only cert

--- ca/ca.Dockerfile
+++ ca/ca.Dockerfile
@@ -5,5 +5,6 @@ RUN apt update && apt install openssl -y
 
 COPY ./hooks.yaml /etc/webhooks/hooks.yaml
 COPY ./*.sh /etc/webhooks/
+RUN chmod +x ./*.sh
 
 CMD ["webhook", "-hooks=/etc/webhooks/hooks.yaml"]
--- ca/create-cert.sh
+++ ca/create-cert.sh
@@ -10,7 +10,7 @@ tmppfx=$(mktemp)
 
 openssl genpkey -algorithm ED25519 -out ${tmpkey}
 openssl req -new -nodes -subj "/C=DE/ST=Baden-Württemberg/L=Mannheim/CN=${1}" -out ${tmpcsr} -key ${tmpkey}
-openssl x509 -req -days 90 -in ${tmpcsr} -CA ca.crt -CAkey ca.key -out ${tmpcrt}
+openssl x509 -req -days 90 -in ${tmpcsr} -CA ca.crt -CAkey ca.key -out ${tmpcrt} > /dev/null 2>&1
 openssl pkcs12 -export -passout pass:kilowattkojote -inkey ${tmpkey} -in ${tmpcrt} -certfile ca.crt -out ${tmppfx}
 
 base64 ${tmppfx}
