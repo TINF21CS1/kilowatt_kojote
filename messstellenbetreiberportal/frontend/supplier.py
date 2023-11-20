02152164cbdcb4cb09930a58c4194352c4767e70
Petzys <87223648+Petzys@users.noreply.github.com>
2023-11-20 14:32:17 +0100

    Remove hardcoded smartmeters list

--- messstellenbetreiberportal/frontend/supplier.py
+++ messstellenbetreiberportal/frontend/supplier.py
@@ -52,36 +52,6 @@ def assign():
             logger.exception(e)
             return render_template('error.html', errors=str(e))
 
-        smartmeters = [
-            {
-                "uuid": "123456789012sgf34567890123456789012",
-                "type": "Smartmeter",
-                "location": "Keller",
-                "supplier": "Stadtwerke",
-                "reading": "1234567890",
-                "usage": "1234567890",
-                "avg_usage": "1234567890"
-            },
-            {
-                "uuid": "23452345",
-                "type": "Smartmeter",
-                "location": "Keller",
-                "supplier": "LOLOL",
-                "reading": "1234567890",
-                "usage": "1234567890",
-                "avg_usage": "1234567890"
-            },
-            {
-                "uuid": "qwerqwerqwer",
-                "type": "Smartmeter",
-                "location": "Keller",
-                "supplier": "LOLOL",
-                "reading": "1234567890",
-                "usage": "1234567890",
-                "avg_usage": "1234567890"
-            },
-        ]
-
         return render_template(
             suppliers = suppliers,
             smartmeters = smartmeters,
