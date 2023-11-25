5435579f91de6a5f10284c1945af86470ac3cbb4
Julian Lemmerich <s212689@student.dhbw-mannheim.de>
2023-11-25 22:28:52 +0000

    fix(smartmeter): run smartmeter.py from cron
    cron is now the foreground process

--- smartmeter/smartmeter_entrypoint.sh
+++ smartmeter/smartmeter_entrypoint.sh
@@ -6,10 +6,10 @@ echo "[+] Requesting a Client Certificate:"
 echo "[#] curl "http://ca.kilowattkojote.de/api/create?name=$(hostname)" | base64 --decode > /app2/cert.pfx"
 curl "http://ca.kilowattkojote.de/api/create?name=$(hostname)" | base64 --decode > /app2/cert.pfx
 
-echo "[#] cron"
-cron
-
-echo "[#] python /app2/init.py"
-python /app2/init.py
 echo "[#] python /app/smartmeter.py"
 python /app/smartmeter.py
+echo "[#] python /app2/init.py"
+python /app2/init.py
+
+echo "[#] cron -f"
+cron -f
