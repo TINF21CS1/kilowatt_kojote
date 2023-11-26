2e3acc26c099b92d0b266310740dcb6827eeef09
Julian Lemmerich <s212689@student.dhbw-mannheim.de>
2023-11-26 21:54:05 +0000

    fix(smartmeter): fix content-type for requests"
    Co-Authored-By: Timm Weber <timm.weber@ibm.com>

--- smartmeter/meter_software/network.py
+++ smartmeter/meter_software/network.py
@@ -4,7 +4,7 @@ from .constants import URL, URL_REGISTER, URL_DATA, CERT_PEM, CERT_KEY
 
 class requests:
     def register(data):
-        return request_lib.post(url=URL+URL_REGISTER, data=data, cert=(CERT_PEM, CERT_KEY))
+        return request_lib.post(url=URL+URL_REGISTER, json=data, cert=(CERT_PEM, CERT_KEY))
 
     def send(data):
-        return request_lib.post(url=URL+URL_DATA, data=data, cert=(CERT_PEM, CERT_KEY))
+        return request_lib.post(url=URL+URL_DATA, json=data, cert=(CERT_PEM, CERT_KEY))
