818dccaeb4bbfa7ef8c13a0a79aeccbbf1173b84
Julian Lemmerich <s212689@student.dhbw-mannheim.de>
2023-12-04 14:32:29 +0000

    fix(db): fix clear_smartemeter script

--- messstellenbetreiberportal/backend/db/clear_smartmeter.py
+++ messstellenbetreiberportal/backend/db/clear_smartmeter.py
@@ -4,10 +4,12 @@ import os
 db_path = os.path.join(os.path.dirname(__file__), "database.db")
 
 if __name__ == "__main__":
-    query = "DELETE FROM Stromzaehler; VACUUM;"
+    query = "DELETE FROM Stromzaehler;"
 
     con = sqlite3.connect(db_path)
     cursor = con.cursor()
 
-    res = cursor.execute(query)
+    cursor.execute(query)
     con.commit()
+    
+    cursor.execute("VACUUM;")
